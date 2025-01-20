512347dfa13c45c3221deda8811d479d050277ef
sourabhpatnaik <sourabhpatnayak19@gmail.com>
2025-01-20 20:35:32 +0530

    Added Basic calculator functionality

--- Js/script.js
+++ Js/script.js
@@ -1,36 +1,86 @@
-let calDisplay = document.querySelector(".calculatorDisplay")
+let calDisplay = document.querySelector(".calculatorDisplay");
+let calButton = document.querySelectorAll("button");
 
-function doAdd(varA, varB) {
-  return varA + varB;
-}
-function doSubtract(varA, varB) {
-  return varA - varB;
-}
-function doMultiply(varA, varB) {
-  return varA * varB;
-}
-function doDivide(varA, varB) {
-  return varA / varB;
-}
-function doMod(varA, varB) {
-  return varA % varB;
-}
+let currentDisplay = "0";
+let resultDisplay = false;
 
-function operate(numOne, numTwo, operator) {
-  if (operator == "+") {
-    return doAdd(numOne, numTwo);
-  } else if (operator == "-") {
-    return doSubtract(numOne, numTwo);
-  } else if (operator == "*") {
-    return doMultiply(numOne, numTwo);
-  } else if (operator == "/") {
-    return doDivide(numOne, numTwo);
-  } else if (operator == "%") {
-    return doMod(numOne, numTwo);
+// function doAdd(varA, varB) {
+//   return varA + varB;
+// }
+// function doSubtract(varA, varB) {
+//   return varA - varB;
+// }
+// function doMultiply(varA, varB) {
+//   return varA * varB;
+// }
+// function doDivide(varA, varB) {
+//   return varA / varB;
+// }
+// function doMod(varA, varB) {
+//   return varA % varB;
+// }
+
+// function operate(numOne, numTwo, operator) {
+//   if (operator == "+") {
+//     return doAdd(numOne, numTwo);
+//   } else if (operator == "-") {
+//     return doSubtract(numOne, numTwo);
+//   } else if (operator == "*") {
+//     return doMultiply(numOne, numTwo);
+//   } else if (operator == "/") {
+//     return doDivide(numOne, numTwo);
+//   } else if (operator == "%") {
+//     return doMod(numOne, numTwo);
+//   } else {
+//     return "Invalid Inputs";
+//   }
+// }
+
+function calculatorDisplay(val) {
+  if (currentDisplay === "0" || resultDisplay) {
+    currentDisplay = val;
   } else {
-    return "Invalid Inputs";
+    currentDisplay += val;
   }
+
+  resultDisplay = false;
+
+  updateDisplay();
 }
 
+function updateDisplay() {
+  calDisplay.textContent = currentDisplay;
+}
 
-calDisplay.textContent = "00"
+function clearDisplay() {
+  currentDisplay = "0";
+  resultDisplay = false;
+
+  updateDisplay();
+}
+
+function clearLastElement() {
+  currentDisplay = currentDisplay.slice(0, -1);
+
+  if (currentDisplay == "") {
+    currentDisplay = "0";
+  }
+
+  updateDisplay();
+}
+
+function calculateResult() {
+  try {
+    const result = eval(currentDisplay);
+
+    currentDisplay += "\n= " + result.toString();
+
+    updateDisplay();
+  } catch (error) {
+    currentDisplay += "\nError";
+
+    updateDisplay();
+  }
+
+  resultDisplay = true;
+}
