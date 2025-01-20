5f33efdc31446127c79633e136061344edbe46ac
sourabhpatnaik <sourabhpatnayak19@gmail.com>
2025-01-20 16:47:53 +0530

    Added Basic Css style for calculator (Not Optimized for Mobile)

--- Js/script.js
+++ Js/script.js
@@ -1,39 +1,36 @@
+let calDisplay = document.querySelector(".calculatorDisplay")
 
-function doAdd(varA,varB){
-    return (varA + varB)
+function doAdd(varA, varB) {
+  return varA + varB;
 }
-function doSubtract(varA,varB){
-    return (varA - varB)
+function doSubtract(varA, varB) {
+  return varA - varB;
 }
-function doMultiply(varA,varB){
-    return (varA * varB)
+function doMultiply(varA, varB) {
+  return varA * varB;
 }
-function doDivide(varA,varB){
-    return (varA / varB)
+function doDivide(varA, varB) {
+  return varA / varB;
 }
-function doMod(varA,varB){
-    return (varA % varB)
-}
-
-
-function operate(numOne, numTwo, operator){
-    if(operator == "+"){
-        return doAdd(numOne,numTwo)
-    }
-    else if(operator == "-"){
-        return doSubtract(numOne,numTwo)
-    }
-    else if(operator == "*"){
-        return doMultiply(numOne,numTwo)
-    }
-    else if(operator == "/"){
-        return doDivide(numOne,numTwo)
-    }
-    else if(operator == "%"){
-        return doMod(numOne,numTwo)
-    }
-    else{
-        return "Invalid Inputs"
-    }
+function doMod(varA, varB) {
+  return varA % varB;
 }
 
+function operate(numOne, numTwo, operator) {
+  if (operator == "+") {
+    return doAdd(numOne, numTwo);
+  } else if (operator == "-") {
+    return doSubtract(numOne, numTwo);
+  } else if (operator == "*") {
+    return doMultiply(numOne, numTwo);
+  } else if (operator == "/") {
+    return doDivide(numOne, numTwo);
+  } else if (operator == "%") {
+    return doMod(numOne, numTwo);
+  } else {
+    return "Invalid Inputs";
+  }
+}
+
+
+calDisplay.textContent = "00"
